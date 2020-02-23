4f37b0dd13656d5a5c8096a9c8b83b91e6ddee23
igiemanalac <igiemanalac@gmail.com>
2020-02-23 14:16:01 +0800

    Added linear regression files

---
+++ LinearRegression.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
+data = pd.read_csv("student-mat.csv", sep=";")
+print(data.head())
+
+# Select needed data/attributes
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+print(data.head())
+
+# Label - What we're looking for
+predict = "G3"
+
+# New data frame without the G3 attribute
+x = np.array(data.drop([predict], 1))
+
+# Labels data frame
+y = np.array(data[predict])
+
+# Take all attributes and labels and split into 4 different arrays
+# Test data will also be created to test the accuracy of the model
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+# Find best fit line with the training data
+linear.fit(x_train, y_train)
+
+# Accuracy of the model
+acc = linear.score(x_test, y_test)
+print(acc)
+
+print("Co: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
