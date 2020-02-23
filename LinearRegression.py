9ff2b0c489b9e621ebdef87a8151ee4f547dd385
igiemanalac <igiemanalac@gmail.com>
2020-02-23 19:13:51 +0800

    Updated linear regression study Added KNN study

--- LinearRegression.py
+++ LinearRegression.py
@@ -23,19 +23,26 @@ x = np.array(data.drop([predict], 1))
 # Labels data frame
 y = np.array(data[predict])
 
-# Take all attributes and labels and split into 4 different arrays
-# Test data will also be created to test the accuracy of the model
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+"""
+acc = 0
+while acc < 0.95:
+    # Take all attributes and labels and split into 4 different arrays
+    # Test data will also be created to test the accuracy of the model
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-# Find best fit line with the training data
-linear.fit(x_train, y_train)
+    # Find best fit line with the training data
+    linear.fit(x_train, y_train)
 
-# Accuracy of the model
-acc = linear.score(x_test, y_test)
-print(acc)
+    # Accuracy of the model
+    acc = linear.score(x_test, y_test)
+    print(acc)
 
+# Save a pickle file that contains the model in the directory
+with open("studentmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)
+    
 print("Co: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
 
@@ -43,3 +50,16 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+"""
+
+# Load the model from the pickle file
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+# Plot data p and its correlation to G3
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
